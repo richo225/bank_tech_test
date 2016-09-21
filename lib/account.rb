a1ab9f278cfdc9b3bb97e03fbf0725d5bc7d0d13
Richard Bates <richo225@gmail.com>
2016-09-21 11:14:31 +0100

    User can deposit money

--- lib/account.rb
+++ lib/account.rb
@@ -6,5 +6,14 @@ class Account
     @balance = 0
   end
 
-  
+  def deposit(value)
+    raise "Please deposit a positive amount" if !positive?(value)
+    @balance += value
+  end
+
+  private
+
+  def positive?(value)
+    value > 0
+  end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -10,4 +10,17 @@ describe Account do
     end
   end
 
+  describe "#deposit" do
+    it "adds value to the initial account balance" do
+      account.deposit(10)
+      expect(account.balance).to eq(10)
+    end
+
+    it "deposit must be greater than zero" do
+      expect{account.deposit(-5)}.to raise_error("Please deposit a positive amount")
+      expect{account.deposit(0)}.to raise_error("Please deposit a positive amount")
+    end
+
+  end
+
 end
