71a5b0217fa0a2dfaca83f597c391df9f040b7c2
Richard Bates <richo225@gmail.com>
2016-09-21 13:39:43 +0100

    Transaction amount can be saved

--- lib/account.rb
+++ lib/account.rb
@@ -1,19 +1,25 @@
 class Account
 
-  attr_accessor :balance
+  attr_accessor :balance, :transactions
 
   def initialize
     @balance = 0
+    @transactions = []
   end
 
   def deposit(value)
     raise "Please deposit a positive amount" if !positive?(value)
+    save_transaction(value)
     @balance += value
   end
 
   def withdraw(value)
     raise "Please withdraw a positive amount" if !positive?(value)
     @balance -= value
+  end
+
+  def save_transaction(value)
+    @transactions << value
   end
 
   private
